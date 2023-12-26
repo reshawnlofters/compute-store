4c202602b438bbd124c066a742e30585e1d64062
Reshawn Lofters <reshawnalofters@gmail.com>
2023-12-25 22:20:17 -0500

    feat(homepage): open a larger view of a product
    - Implemented functionality to open a larger view of a product when a user clicks
      on a product image in the featured products section on the homepage.
    - Created `openProductPopup` and `closeProductPopup` functions to manage the popup's visibility.
    - Created `handleProductImageClick` function to extract the product ID from the clicked image,
      allowing the generation of specific product popup HTML.
    - Included a close button in the popup for user convenience.

--- scripts/home.js
+++ scripts/home.js
@@ -3,6 +3,7 @@ import { formatCurrency } from './utils/format-currency.js';
 import { addProductToCart, calculateCartQuantity } from '../data/checkout-page.js';
 
 const productsGrid = document.querySelector('.products-grid');
+const productPopupContainer = document.querySelector('.product-popup-container');
 
 function generateProductsHTML() {
     let productsHTML = '';
@@ -205,3 +206,57 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+function openProductPopup(matchingProduct) {
+    generateProductPopup(matchingProduct.id);
+    productPopupContainer.style.display = 'block';
+}
+
+function closeProductPopup() {
+    productPopupContainer.style.display = 'none';
+}
+
+function generateProductPopup(productId) {
+    const matchingProduct = products.find((product) => product.id === productId);
+
+    if (!matchingProduct) {
+        console.error(`Product with ID ${productId} not found.`);
+        return;
+    }
+
+    if (productPopupContainer) {
+        let productPopupHTML = `
+            <div class="product-popup-image">
+                <img src="${matchingProduct.image}" alt="${matchingProduct.name}">
+            </div>
+            <button class="close-product-popup-button">
+                <i class="bi bi-x-lg"></i>
+            </button>`;
+
+        productPopupContainer.innerHTML = productPopupHTML;
+
+        // Event listener for the close button
+        const closeProductPopupButton = productPopupContainer.querySelector('.close-product-popup-button');
+        if (closeProductPopupButton) {
+            closeProductPopupButton.addEventListener('click', closeProductPopup);
+        }
+    }
+}
+
+function handleProductImageClick(event) {
+    const productImageContainer = event.target.closest('.product-image-container');
+
+    if (productImageContainer) {
+        const productName = productImageContainer
+            .closest('.product-container')
+            .querySelector('.product-name')
+            .textContent.trim();
+        const matchingProduct = products.find((product) => product.name === productName);
+
+        if (matchingProduct) {
+            openProductPopup(matchingProduct);
+        }
+    }
+}
+
+productsGrid.addEventListener('click', handleProductImageClick);
